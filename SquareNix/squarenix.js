bc2415e589e5b0a5cb883ad3772037fc2557d958
Gabriel <gabrieltsf10@gmail.com>
2022-02-04 19:44:05 -0300

    SquareNix | New font

--- SquareNix/squarenix.js
+++ SquareNix/squarenix.js
@@ -1,26 +1,26 @@
 window.addEventListener('load', function(){
     const diaDaSemana = [
-        'Domingo',
-        'Segunda-Feira',
-        'Terça-Feira',
-        'Quarta-Feira',
-        'Quinta-Feira',
-        'Sexta-Feira',
-        'Sabado'
+        'Sunday',
+        'Monday',
+        'Tuesday',
+        'Wednesday',
+        'Thursday',
+        'Friday',
+        'Saturday'
     ]
     const Mes = [
-        'Janeiro',
-        'Fevereiro',
-        'Março',
-        'Abril',
-        'Maio',
-        'Junho',
-        'Julho',
-        'Agosto',
-        'Setembro',
-        'Outubro',
-        'Novembro',
-        'Dezembro'
+        'January',
+        'February',
+        'March',
+        'April',
+        'May',
+        'June',
+        'July',
+        'August',
+        'September',
+        'October',
+        'November',
+        'December'
     ]
     const dateTime = new Date();
     var time = document.getElementById('date');
@@ -42,6 +42,6 @@ window.addEventListener('load', function(){
         return (hours < 10 ? "0" +hours : hours) + ':' + (minutes < 10 ? "0" + minutes : minutes)+ ':' + (seconds < 10 ? "0" + seconds : seconds);
     }
     function getDate(dateTime){
-        return diaDaSemana[dateTime.getDay()] + " - " + dateTime.getDate() + " de " + Mes[dateTime.getMonth()];
+        return diaDaSemana[dateTime.getDay()] + " - " + Mes[dateTime.getMonth()]  + "  " + dateTime.getDate();
     }
 })
